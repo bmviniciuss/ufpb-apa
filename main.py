b1f25fdf40c45f49e73541fd8171fa7f517a22f0
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-03-12 17:17:35 -0300

    refactor(reinsertion): adding the reinsertion to a file

--- main.py
+++ main.py
@@ -4,27 +4,7 @@ from utils import *
 from reader import *
 from Guloso import Guloso
 from datetime import datetime
-from math import inf
-
-def reinsertion(routes, matrix_cost, cost):
-  mRemocao = inf
-  mInsercao = 0
-  auxCost = inf
-  mDistancia = cost
-  
-  for r in range(len(routes)):
-    for i in range(1,len(routes[r])):
-      for k in range(i + 1, len(routes[r]) - 1):
-        route = routes[r]
-        auxCost = cost -matrix_cost[route[i-1]][route[i]] -matrix_cost[route[i]][route[i + 1]]  -matrix_cost[route[k]][route[k + 1]]  +matrix_cost[route[i - 1]][route[i + 1]]  +matrix_cost[route[k]][route[i]]  +matrix_cost[route[i]][route[k+1]] 
-
-
-        if auxCost < mDistancia:
-          mDistancia = auxCost
-
-  return mDistancia
-
-
+from optimizations import reinsertion
 
 def main():
   # Input Parser+
